2ebc80fb16d8b193a3ab8946473ce7e6455f3db8
unknown <muriraubach@gmail.com>
2021-11-14 10:27:23 -0300

    Feat: add the dashboard to the components

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,32 @@
+import { Container } from "./styles";
+import incomeImg from '../../assets/income.svg'
+import outcomeIgm from '../../assets/outcome.svg'
+import totalIgm from '../../assets/total.svg'
+
+export function Summary() {
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={incomeImg} alt="Entradas" />
+        </header>
+        <strong>R$ 17.400,00</strong>
+      </div>
+      <div>
+        <header>
+          <p>Saídas</p>
+          <img src={outcomeIgm} alt="Saídas" />
+        </header>
+        <strong>R$ 1.259,00</strong>
+      </div>
+      <div className="highlight-background">
+        <header>
+          <p>Total</p>
+          <img src={totalIgm} alt="Total" />
+        </header>
+        <strong>R$ 16.141,00</strong>
+      </div>
+    </Container>
+  )
+}
